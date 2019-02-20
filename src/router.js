6345b59f87890855dc6dc27f4cc7036355353bd4
zhouhuafei <1123486116@qq.com>
2019-02-20 16:18:34 +0800

    init

---
+++ src/router.js
@@ -0,0 +1,22 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+
+Vue.use(Router);
+
+export default new Router({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home,
+        },
+        {
+            path: '*',
+            name: '404',
+            component: () => import(/* webpackChunkName: "404" */ './views/404.vue'),
+        },
+    ],
+});
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="home">
+        123
+    </div>
+</template>
+
+<script>
+    // @ is an alias to /src
+    import {
+        Button,
+        Cell,
+    } from 'vant';
+
+    export default {
+        name: 'home',
+        components: {
+            Button,
+            Cell,
+        },
+    };
+</script>
