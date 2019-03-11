f2852d8a3565b0dcb8f6129a395a882f58e97a29
zhouhuafei <1123486116@qq.com>
2019-03-11 18:48:23 +0800

    md

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -97,8 +97,13 @@ export default function (opts) {
                 });
                 */
             }
-            // 这里我个人建议返回一个{status:'error'}。否则后续使用async/await时如果接口报错会导致js报错。
-            reject(error);
+            // 这里我个人建议返回格式如下。否则后续使用async/await时如果接口报错会导致js报错。
+            reject({
+                data: {
+                    status: 'error',
+                    message: error,
+                },
+            });
         });
     }));
 }
