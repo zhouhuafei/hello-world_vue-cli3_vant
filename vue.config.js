6cc51975f36730956eb7064c74d690fd548384ea
zhouhuafei <1123486116@qq.com>
2019-03-24 19:16:52 +0800

    md

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,7 @@
 const isDevelopment = process.env.NODE_ENV === 'development';
 
 module.exports = {
-    baseUrl: '/base-url',
-    // publicPath: isDevelopment ? '/' : '/',
+    publicPath: isDevelopment ? '/base-url/' : '/base-url/',
     devServer: {
         proxy: {
             '/api': {
