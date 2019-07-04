ec9dd0d52025d4926e645fbe0a64ba0c9fd85d1e
zhouhuafei <zhouhuafei@wowkai.cn>
2019-07-04 21:17:10 +0800

    vinyl-fs

--- cut.js
+++ cut.js
@@ -1,6 +1,19 @@
-const envDirAdd = ['alpha', 'beta'];
-const envDirNow = process.argv[2] || '';
+const vfs = require('vinyl-fs'); // 我从gulp包的依赖中找到的这个包
+const del = require('del');
 
-if (envDirNow) {
-    console.log(envDirNow);
+const envDirBakOrMoveOrDel = process.argv[2] || ''; // 是备份还是移动或者是删除？
+const envDirTarget = process.argv[3] || ''; // 备份或移动到哪里或者是删除哪个目录？
+
+if (envDirBakOrMoveOrDel && envDirTarget) {
+    if (envDirBakOrMoveOrDel === 'bak') { // 备份
+        vfs.src(['./dist/**/**'])
+            .pipe(vfs.dest(`./dist-bak/${envDirTarget}`));
+    }
+    if (envDirBakOrMoveOrDel === 'move') { // 移动
+        vfs.src(['./dist-bak/**/**'])
+            .pipe(vfs.dest('./dist/'));
+    }
+    if (envDirBakOrMoveOrDel === 'del') { // 删除
+        del.sync(['./dist-bak/']);
+    }
 }
